c38abe62e07da8560a17807b9a01883248fb54e2
k4t4n0-ux <k4t4n0@gmail.com>
2026-02-10 13:36:41 +0100

    multiclase

--- Java/subclases.js
+++ Java/subclases.js
@@ -1,38 +1,55 @@
-const subclases = {
-    artificiero: ["Artificiero 1", "Artificiero 2", "Artificiero 3"],
-    barbaro: ["Bárbaro 1", "Bárbaro 2"],
-    bardo: ["Bardo 1", "Bardo 2", "Bardo 3"],
-    clerigo: ["Clérigo 1", "Clérigo 2"],
-    druida: ["Druida 1", "Druida 2"],
-    luchador: ["Luchador 1", "Luchador 2", "Luchador 3"],
-    monje: ["Monje 1", "Monje 2"],
-    paladin: ["Paladín 1", "Paladín 2"],
-    explorador: ["Explorador 1", "Explorador 2"],
-    picaro: ["Pícaro 1", "Pícaro 2"],
-    hechicero: ["Hechicero 1", "Hechicero 2"],
-    brujo: ["Brujo 1", "Brujo 2"],
-    mago: ["Mago 1", "Mago 2", "Mago 3"],
-    cazador_sangre: ["Cazador 1", "Cazador 2"],
-    psionico: ["Psiónico 1", "Psiónico 2"]
-};
+document.addEventListener("DOMContentLoaded", function () {
 
-const claseSelect = document.getElementById("clase");
-const subclaseSelect = document.getElementById("subclase");
+    const clase1 = document.getElementById("clase");
+    const subclase1 = document.getElementById("subclase");
 
-claseSelect.addEventListener("change", function() {
+    const clase2 = document.getElementById("clase2");
+    const subclase2 = document.getElementById("subclase2");
 
-    const claseSeleccionada = this.value;
+    const subclasesPorClase = {
 
-    // Reiniciar subclases
-    subclaseSelect.innerHTML = '<option value="">-- Subclase --</option>';
+        barbaro: ["Camino del Berserker", "Camino del Tótem"],
+        bardo: ["Colegio del Saber", "Colegio del Valor"],
+        clerigo: ["Dominio de la Vida", "Dominio de la Guerra"],
+        druida: ["Círculo de la Luna", "Círculo de la Tierra"],
+        luchador: ["Campeón", "Maestro de Batalla"],
+        paladin: ["Juramento de Devoción", "Juramento de Venganza"],
+        picaro: ["Ladrón", "Asesino"],
+        hechicero: ["Linaje Dracónico", "Magia Salvaje"],
+        mago: ["Evocación", "Ilusión"],
+        monje: ["Camino de la Mano Abierta", "Camino de la Sombra"],
+        explorador: ["Cazador", "Maestro de Bestias"],
+        brujo: ["El Archifey", "El Infernal"],
+        artificiero: ["Alquimista", "Armero"],
+        cazador_sangre: ["Orden del Fantasma", "Orden del Profano"],
+        psionico: ["Telepata", "Telequinético"]
+    };
 
-    if (subclases[claseSeleccionada]) {
-        subclases[claseSeleccionada].forEach(function(sub) {
-            const option = document.createElement("option");
-            option.value = sub;
-            option.textContent = sub;
-            subclaseSelect.appendChild(option);
-        });
+    function actualizarSubclases(selectClase, selectSubclase) {
+
+        const claseSeleccionada = selectClase.value;
+
+        selectSubclase.innerHTML = '<option value="">-- Subclase --</option>';
+
+        if (subclasesPorClase[claseSeleccionada]) {
+
+            subclasesPorClase[claseSeleccionada].forEach(nombre => {
+
+                const option = document.createElement("option");
+                option.value = nombre.toLowerCase().replace(/\s+/g, "_");
+                option.textContent = nombre;
+
+                selectSubclase.appendChild(option);
+            });
+        }
     }
+
+    clase1.addEventListener("change", function () {
+        actualizarSubclases(clase1, subclase1);
+    });
+
+    clase2.addEventListener("change", function () {
+        actualizarSubclases(clase2, subclase2);
+    });
 
 });

9cb4177b38e2e6e5ecce93e99c5dad5f4b914e59
k4t4n0-ux <k4t4n0@gmail.com>
2026-02-25 10:31:11 +0100

    armas

--- Java/armas.js
+++ Java/armas.js
@@ -14,37 +14,31 @@ const armasDB = {
         },
         "Bastón": {
             dano: "1d6 Contundente",
-            distancia: "Toque",
             propiedad: "Versatil (1d8)",
             caracteristica: "fuerza"
         },
         "Clava": {
             dano: "1d4 Contundente",
-            distancia: "Toque",
             propiedad: "Ligera",
             caracteristica: "fuerza"
         },
         "Daga": {
             dano: "1d4 Perforante",
-            distancia: "Toque",
             propiedad: "Arrojadiza (20/60), Ligera, Sutil",
             caracteristica: "fuerza_dest"
         },
         "Dardo": {
             dano: "1d4 Perforante",
-            distancia: "Toque",
             propiedad: "Arrojadiza (20/60), Sutil",
             caracteristica: "fuerza_dest"
         },
         "Gran Clava": {
             dano: "1d8 Contundente",
-            distancia: "Toque",
             propiedad: "Dos Manos",
             caracteristica: "fuerza"
         },
         "Hacha de Mano": {
             dano: "1d6 Cortante",
-            distancia: "Toque",
             propiedad: "Arrojadiza (20/60), Ligera",
             caracteristica: "fuerza"
         },
@@ -56,31 +50,26 @@ const armasDB = {
         },
         "Hoz": {
             dano: "1d4 Cortante",
-            distancia: "Toque",
             propiedad: "Ligera",
             caracteristica: "fuerza"
         },
         "Jabalina": {
             dano: "1d6 Perforante",
-            distancia: "Toque",
             propiedad: "Arrojadiza (30/120)",
             caracteristica: "fuerza"
         },
         "Lanza": {
             dano: "1d6 Perforante",
-            distancia: "Toque",
             propiedad: "Arrojadiza (20/60), Versátil (1d8)",
             caracteristica: "fuerza"
         },
         "Martillo Ligero": {
             dano: "1d4 Contundente",
-            distancia: "Toque",
             propiedad: "Arrojadiza (20/60), Ligera",
             caracteristica: "fuerza"
         },
         "Maza": {
             dano: "1d6 Contundente",
-            distancia: "Toque",
             propiedad: "",
             caracteristica: "fuerza"
         }
@@ -89,13 +78,11 @@ const armasDB = {
     marcial: {
         "Espada Corta": {
             dano: "1d6",
-            distancia: "Toque",
             propiedad: "Sutil",
             caracteristica: "fuerza_dest"
         },
         "Espada Larga": {
             dano: "1d8",
-            distancia: "Toque",
             propiedad: "Versátil (1d10)",
             caracteristica: "fuerza"
         },

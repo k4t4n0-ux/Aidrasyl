2313387033a3c1e68ac7b16ce3eab4c4e28fbc73
k4t4n0-ux <k4t4n0@gmail.com>
2026-02-25 10:47:25 +0100

    arma

--- Java/armas.js
+++ Java/armas.js
@@ -3,93 +3,118 @@ const armasDB = {
         "Arco Corto": {
             dano: "1d6 Perforante",
             distancia: "80/320",
-            propiedad: "Dos Manos, Munición",
+            propiedad: "Dos Manos",
             caracteristica: "destreza"
         },
         "Ballesta Ligera": {
             dano: "1d8 Perforante",
             distancia: "80/320",
-            propiedad: "De carga, Dos Manos, Munición",
+            propiedad: "De carga, Dos Manos",
             caracteristica: "destreza"
         },
         "Bastón": {
             dano: "1d6 Contundente",
+            distancia: "",
             propiedad: "Versatil (1d8)",
             caracteristica: "fuerza"
         },
         "Clava": {
             dano: "1d4 Contundente",
+            distancia: "",
             propiedad: "Ligera",
             caracteristica: "fuerza"
         },
         "Daga": {
             dano: "1d4 Perforante",
+            distancia: "",
             propiedad: "Arrojadiza (20/60), Ligera, Sutil",
             caracteristica: "fuerza_dest"
         },
         "Dardo": {
             dano: "1d4 Perforante",
+            distancia: "",
             propiedad: "Arrojadiza (20/60), Sutil",
             caracteristica: "fuerza_dest"
         },
         "Gran Clava": {
             dano: "1d8 Contundente",
+            distancia: "",
             propiedad: "Dos Manos",
             caracteristica: "fuerza"
         },
         "Hacha de Mano": {
             dano: "1d6 Cortante",
+            distancia: "",
             propiedad: "Arrojadiza (20/60), Ligera",
             caracteristica: "fuerza"
         },
         "Honda": {
             dano: "1d4 Contundente",
             distancia: "30/120",
-            propiedad: "Munición",
+            propiedad: "",
             caracteristica: "destreza"
         },
         "Hoz": {
             dano: "1d4 Cortante",
+            distancia: "",
             propiedad: "Ligera",
             caracteristica: "fuerza"
         },
         "Jabalina": {
             dano: "1d6 Perforante",
+            distancia: "",
             propiedad: "Arrojadiza (30/120)",
             caracteristica: "fuerza"
         },
         "Lanza": {
             dano: "1d6 Perforante",
+            distancia: "",
             propiedad: "Arrojadiza (20/60), Versátil (1d8)",
             caracteristica: "fuerza"
         },
         "Martillo Ligero": {
             dano: "1d4 Contundente",
+            distancia: "",
             propiedad: "Arrojadiza (20/60), Ligera",
             caracteristica: "fuerza"
         },
         "Maza": {
             dano: "1d6 Contundente",
+            distancia: "",
             propiedad: "",
             caracteristica: "fuerza"
         }
 
     },
     marcial: {
-        "Espada Corta": {
-            dano: "1d6",
-            propiedad: "Sutil",
-            caracteristica: "fuerza_dest"
+        "Alabarda": {
+            dano: "1d10 Cortante",
+            distancia: "5",
+            propiedad: "Dos Manos, Pesada",
+            caracteristica: "fuerza"
+        },
+        "Arco Largo": {
+            dano: "1d8 Perforante",
+            distancia: "150/600",
+            propiedad: "Dos Manos, Pesada",
+            caracteristica: "destreza"
+        },
+        "Ballesta de Mano": {
+            dano: "1d6 Perforante",
+            distancia: "30/120",
+            propiedad: "De carga, Ligera",
+            caracteristica: "destreza"
         },
         "Espada Larga": {
             dano: "1d8",
+            distancia: "Toque",
             propiedad: "Versátil (1d10)",
             caracteristica: "fuerza"
         },
         "Arco Largo": {
             dano: "1d8",
             distancia: "150/600",
-            propiedad: "Munición, Dos Manos",
+            propiedad: "Dos Manos",
             caracteristica: "destreza"
         }
     }

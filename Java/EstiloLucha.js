4cb54e1da4421db195c3db0660afc5b0ffde9235
k4t4n0-ux <k4t4n0@gmail.com>
2026-02-26 16:54:08 +0100

    scsdccssccds

--- Java/EstiloLucha.js
+++ Java/EstiloLucha.js
@@ -1,46 +1,66 @@
 const estilosLuchaDB = {
 
-    "Combate con Dos Armas": {
+    "Tiro con arco": {
         descripcion: `
-Cuando luchas con dos armas, puedes añadir tu modificador de característica al daño del segundo ataque.
+Obtienes una bonificación de +2 por las tiradas de ataque que haces con armas a distancia.
+        `
+    },
+
+    "Lucha ciega": {
+        descripcion: `
+Tienes visión ciega con un alcance de 10 pies.
         `
     },
 
     "Defensa": {
         descripcion: `
-Mientras lleves armadura, obtienes un +1 a tu Clase de Armadura.
+Mientras usas armadura ligera, media o pesada, obtienes una bonificación de +1 para la clase de armadura.
         `
     },
 
     "Duelo": {
         descripcion: `
-Cuando empuñas un arma cuerpo a cuerpo en una mano y no usas otra arma, obtienes un +2 a las tiradas de daño con esa arma.
+Cuando sostienes un arma cuerpo a cuerpo en una mano y ninguna otra, obtienes una bonificación de +2 para dañar las tiradas con esa arma.
         `
     },
 
-    "Gran Arma": {
+    "Gran lucha con armas": {
         descripcion: `
-Cuando obtienes un 1 o 2 en un dado de daño de un ataque realizado con un arma cuerpo a cuerpo que estés empuñando con dos manos, puedes repetir el dado y debes usar el nuevo resultado.
+Cuando lanzas daño para un ataque que haces con un arma cuerpo a cuerpo que estás sosteniendo con las dos manos, puedes tratar cualquier 1 o 2 en un dado de daño como un 3. El arma debe tener la propiedad de dos manos o versátil para obtener este beneficio.
         `
     },
 
     "Intercepción": {
         descripcion: `
-Cuando una criatura que puedes ver golpea a un objetivo que no seas tú y que esté a 5 pies de ti, puedes usar tu reacción para reducir el daño en 1d10 + tu Bono de Competencia. Debes estar empuñando un arma o escudo.
+Cuando una criatura que puedes ver golpea a otra criatura a menos de 5 pies de ti con una tirada de ataque, puedes tomar una reacción para reducir el daño infligido al objetivo en 1d10 más tu bonificación de competencia. Debes tener un Escudo o un arma Simple o Marcial para usar esta Reacción.
         `
     },
 
     "Protección": {
         descripcion: `
-Cuando una criatura que puedes ver ataca a un objetivo que no seas tú y que esté a 5 pies de ti, puedes usar tu reacción para imponer desventaja en la tirada de ataque. Debes estar empuñando un escudo.
+Cuando una criatura que puedes ver ataca a un objetivo distinto de ti que está a 5 pies de ti, puedes tomar una reacción para interponer tu escudo si estás sosteniendo uno. Impones una desventaja en la tirada de ataque desencadenante y en todas las demás tiradas de ataque contra el objetivo hasta el comienzo de tu siguiente turno si permaneces a menos de 5 pies del objetivo.
         `
     },
 
-    "Tiro con Arco": {
+    "Lucha con armas arrojadizas": {
         descripcion: `
-Obtienes un +2 a las tiradas de ataque que realices con armas a distancia.
+Cuando golpeas con una tirada de ataque a distancia usando un arma que tiene la propiedad Lanzada, obtienes una bonificación de +2 en la tirada de daño.
         `
-    }
+    },
+
+    "Lucha con dos armas": {
+        descripcion: `
+Cuando realizas un ataque adicional como resultado del uso de un arma que tiene la propiedad Luz, puedes agregar tu modificador de habilidad al daño de ese ataque si aún no lo estás agregando al daño.
+        `
+    },
+
+    "Lucha desarmada": {
+        descripcion: `
+Cuando golpeas con tu Golpe desarmado y infliges daño, puedes infligir un daño contundente igual a 1d6 más tu modificador de fuerza en lugar del daño normal de un Golpe desarmado. Si no tienes armas ni un escudo cuando haces la tirada de ataque, el d6 se convierte en un d8.
+
+Al comienzo de cada uno de tus turnos, puedes infligir un daño contundente de 1d4 a una criatura atrapada por ti.
+        `
+    },
 
 };
 

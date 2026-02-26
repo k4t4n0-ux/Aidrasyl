4a567f4455194a42ed65ef14d76a0266dce02eb4
k4t4n0-ux <k4t4n0@gmail.com>
2026-02-26 11:24:28 +0100

    sdcs

--- Java/doteTransfondo.js
+++ Java/doteTransfondo.js
@@ -1,88 +1,76 @@
 const dotesDB = {
 
-    "Iniciado Mágico": {
-        descripcion: `
-Aprendes dos trucos de tu elección de la lista de hechizos de Clérigo, Druida o Mago. Inteligencia, Sabiduría o Carisma es tu característica para lanzar estos hechizos (eliges al seleccionar esta dote).
-
-Elige un hechizo de nivel 1 de la misma lista que seleccionaste para los trucos. Siempre tienes ese hechizo preparado. Puedes lanzarlo una vez sin gastar espacio de conjuro y recuperas esta capacidad al finalizar un descanso largo. También puedes lanzarlo usando cualquier espacio de conjuro que tengas.
-
-Cada vez que obtienes un nivel, puedes reemplazar uno de los hechizos elegidos por otro diferente del mismo nivel de la lista seleccionada.
-
-Puedes escoger esta dote más de una vez, pero debes elegir una lista de hechizos diferente cada vez.
-        `
-    },  
-
-    "Resistente": {
-        descripcion: `
-Aumenta una característica a tu elección en 1.
-
-Obtienes competencia en las tiradas de salvación de esa característica.
-        `
-    },
-
     "Alerta": {
         descripcion: `
-Competencia en iniciativas.
-Cuando lanzas Iniciativa, puedes agregar tu Bono de Competencia al resultado.
+Competencia en iniciativas. Cuando lanza Iniciativa, puede agregar su Bono de Competencia a la lista.
 
-Intercambio de iniciativas.
-Inmediatamente después de lanzar Iniciativa, puedes intercambiar tu resultado con el de un aliado dispuesto en el mismo combate. No puedes realizar este intercambio si tú o el aliado tienen la condición Incapacitado.
+Intercambio de iniciativas. Inmediatamente después de lanzar Iniciativa, puedes intercambiar tu Iniciativa con la Iniciativa de un aliado dispuesto en el mismo combate. No puedes realizar este intercambio si tú o el aliado tienes la condición de Incapacitado.
         `
     },
 
     "Artesano": {
         descripcion: `
-Competencia en herramientas.
-Obtienes competencia con tres herramientas artesanales diferentes de tu elección de la mesa Fast Crafting.
+Competencia en herramientas. Obtienes competencia con tres herramientas artesanales diferentes de tu elección de la mesa Fast Crafting.
 
-Descuento.
-Cada vez que compras un artículo no mágico, recibes un descuento del 20%.
+Descuento. Cada vez que compras un artículo no mágico, recibes un descuento del 20 por ciento.
 
-Elaboración rápida.
-Cuando terminas un descanso largo, puedes crear una pieza de equipo de la mesa de elaboración rápida, siempre que tengas las herramientas asociadas y seas competente con ellas. El artículo dura hasta que terminas otro descanso largo, momento en el cual se desmorona.
+Elaboración rápida. Cuando terminas un descanso largo, puedes crear una pieza de equipo de la mesa de elaboración rápida, siempre que tengas las herramientas del artesano asociadas con ese artículo y domines esas herramientas. El artículo dura hasta que terminas otro Descanso Largo, momento en el cual el artículo se desmorona.
 
-Elaboración rápida:
-Herramientas de carpintero: Escalera, Antorcha
-Herramientas de peletero: Estuche, Bolsa
-Herramientas de Mason: Bloquear y abordar
-Herramientas de alfarero: Jarra, Lámpara
-Herramientas de Smith: Rodamientos de bolas, Cubo, Abrojos, Gancho de agarre, Olla de hierro
-Herramientas de Tinker: Campana, Pala, Caja de Yesca
-Herramientas del tejedor: Cesta, Cuerda, Red, Tienda
-Herramientas del tallador de madera: Garrote, Gran Garrote, Bastón
+Herramientas de carpintero:	Escalera, Antorcha
+Herramientas de peletero:	Estuche, bolsa
+Herramientas de Mason:	Bloquear y abordar
+Herramientas de alfarero:	Jarra, Lámpara
+Herramientas de Smith:	Rodamientos de bolas, Cubo, Abrojos, Gancho de agarre, Olla de hierro
+Herramientas de Tinker:	Campana, Pala, Caja de yesca
+Herramientas del tejedor:	Cesta, Cuerda, Red, Tienda
+Herramientas del tallador de madera:	Clava, Gran clava, Bastón
         `
     },
 
     "Curandero": {
         descripcion: `
-Médico de batalla.
-Si tienes un kit de sanador, puedes gastar un uso y atender a una criatura a menos de 5 pies como acción. Esa criatura puede gastar uno de sus Dados de Golpe, tirar ese dado y recuperar puntos de vida iguales al resultado más tu Bono de Competencia.
+Médico de batalla. Si tienes un kit de sanador, puedes gastar un uso de él y atender a una criatura a menos de 5 pies de ti mismo como acción de uso. Esa criatura puede gastar uno de sus dados de golpe. La criatura recupera una cantidad de puntos de vida igual a la tirada más tu bonificación de competencia.
 
-Reenrolls curativos.
-Cada vez que tiras un dado para determinar la cantidad de puntos de vida restaurados con un hechizo o con Médico de batalla, puedes repetir el dado si obtienes un 1 y debes usar la nueva tirada.
+Reenrolls curativos. Cada vez que sacas un dado para determinar la cantidad de puntos de vida que restauras con un hechizo o con el beneficio Battle Medic de esta hazaña, puedes volver a tirar el dado si saca un 1 y debes usar la nueva tirada.        `
+    },
+
+    "Afortunado": {
+        descripcion: `
+Puntos de suerte. Tiene una cantidad de puntos de suerte igual a su bono de competencia y puede gastar los puntos en los beneficios a continuación. Recuperas los puntos de suerte gastados cuando terminas un descanso prolongado.
+
+Ventaja. Cuando sacas un d20 para una prueba D20, puedes gastar 1 punto de suerte para darte una ventaja en el rollo.
+
+Desventaja. Cuando una criatura saca un d20 para atacar a ti, puedes gastar 1 punto de suerte para imponer desventaja en el rollo.
+        `
+    },
+
+    "Curandero": {
+        descripcion: `
+Médico de batalla. Si tienes un kit de sanador, puedes gastar un uso y atender a una criatura a menos de 5 pies como acción. Esa criatura puede gastar uno de sus Dados de Golpe, tirar ese dado y recuperar puntos de vida iguales al resultado más tu Bono de Competencia.
+
+Reenrolls curativos. Cada vez que tiras un dado para determinar la cantidad de puntos de vida restaurados con un hechizo o con Médico de batalla, puedes repetir el dado si obtienes un 1 y debes usar la nueva tirada.
         `
     },
 
     "Afortunado": {
         descripcion: `
-Puntos de suerte.
-Tienes una cantidad de puntos de suerte igual a tu Bono de Competencia. Recuperas todos los puntos gastados cuando terminas un descanso largo.
+Puntos de suerte. Tienes una cantidad de puntos de suerte igual a tu Bono de Competencia. Recuperas todos los puntos gastados cuando terminas un descanso largo.
 
-Ventaja.
-Cuando realizas una tirada d20, puedes gastar 1 punto de suerte para darte ventaja.
+Ventaja. Cuando realizas una tirada d20, puedes gastar 1 punto de suerte para darte ventaja.
 
-Desventaja.
-Cuando una criatura realiza una tirada d20 para atacarte, puedes gastar 1 punto de suerte para imponer desventaja.
+Desventaja. Cuando una criatura tira un d20 para una tirada de ataque contra ti, puedes gastar 1 punto de suerte para imponer una desventaja en esa tirada.
         `
     },
 
-    "Músico": {
+    "Iniciado mágico": {
         descripcion: `
-Entrenamiento de instrumentos.
-Obtienes competencia con tres instrumentos musicales de tu elección.
+Dos Trucos. Aprendes dos trucos de tu elección del Clérigo, Druida, o Mago lista de hechizos. Inteligencia, Sabiduría o Carisma es tu habilidad para lanzar hechizos para los hechizos de esta hazaña (elige cuando seleccionas este dote).
 
-Canción alentadora.
-Al terminar un descanso corto o largo, puedes tocar una canción en un instrumento con el que seas competente y otorgar Inspiración Heroica a aliados que la escuchen. El número de aliados es igual a tu Bono de Competencia.
+Hechizo de nivel 1. Elija un hechizo de nivel 1 de la misma lista que seleccionó para los trucos de esta hazaña. Siempre tienes ese hechizo preparado. Puedes lanzarlo una vez sin un espacio para hechizos y recuperas la capacidad de lanzarlo de esa manera cuando terminas un descanso largo. También puedes lanzar el hechizo usando cualquier espacio para hechizos que tengas.
+
+Cambio de hechizo. Cada vez que obtienes un nuevo nivel, puedes reemplazar uno de los hechizos que elegiste para esta hazaña con un hechizo diferente del mismo nivel de la lista de hechizos elegidos.
+
+Repetible. Puedes realizar esta hazaña más de una vez, pero debes elegir una lista de hechizos diferente cada vez.
         `
     },
 
@@ -137,7 +125,7 @@ if (bloque) {
         <div class="dote-container">
 
             <div class="dote-header">
-                <strong>Dote de Transfondo</strong>
+                <strong>Dote de Origen</strong>
                 <button class="toggleDote">▼</button>
             </div>
 

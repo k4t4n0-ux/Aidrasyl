47a383f1c838f29805bb2e4bf56353b43ed512b8
k4t4n0-ux <k4t4n0@gmail.com>
2026-04-07 12:52:58 +0200

    da

---
+++ Java/index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botones = document.querySelectorAll(".menu-secciones button");
+    const secciones = document.querySelectorAll(".seccion");
+
+    function cambiarSeccion(nombre) {
+
+        // ocultar todas
+        secciones.forEach(sec => {
+            sec.classList.remove("activa");
+        });
+
+        botones.forEach(btn => {
+            btn.classList.remove("activo");
+        });
+
+        // activar la correcta
+        const seccionActiva = document.querySelector(`.seccion[data-seccion="${nombre}"]`);
+        const botonActivo = document.querySelector(`button[data-seccion="${nombre}"]`);
+
+        if (seccionActiva) seccionActiva.classList.add("activa");
+        if (botonActivo) botonActivo.classList.add("activo");
+    }
+
+    // listeners botones
+    botones.forEach(btn => {
+        btn.addEventListener("click", () => {
+            cambiarSeccion(btn.dataset.seccion);
+        });
+    });
+
+    // 🔥 sección inicial
+    cambiarSeccion("combate");
+
+});
